5275fe366d31aef321ff7c2021a1ecda08766452
robin <iamufather@qq.com>
2016-05-19 15:33:02 +0800

    Javascipt Dom编程艺术

---
+++ A.4/text.js
@@ -0,0 +1,7 @@
+/*
+* @Author: robin
+* @Date:   2016-05-18 13:27:24
+* @Last Modified by:   robin
+* @Last Modified time: 2016-05-19 13:55:04
+*/
+$('<div id="example">Hello</div>').appendTo(document.body);
